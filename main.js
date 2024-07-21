0d146806f5b05c918d9214b5aecdee26e5bd67ce
dayvancowboy13 <chris.e.friedrich@gmail.com>
2024-07-21 10:25:23 -0700

    append and prepend functions working; starting on at and pop functions

--- main.js
+++ main.js
@@ -40,11 +40,66 @@ class LinkedList {
 
     }
 
+    prepend(value) {
+
+        let node = new Node(value);
+        if (this.size === 0) {
+
+            this.head = node;
+            this.tail = node;
+
+        } else {
+
+            node.nextNode = this.head;
+            this.head = node;
+
+        }
+
+        this.size++;
+
+    }
+
+    at(index) {
+        // return the node at provided index
+
+    }
+
+    pop() {
+
+        if (this.size !== 0) {
+            // probably have to use the at() function
+        }
+
+    }
+
+    get size() {
+
+        return this.size;
+
+    }
+
+    get head() {
+
+        return this.head;
+
+    }
+
+    get tail() {
+
+        return this.tail;
+
+    }
+
 }
 
 const l1 = new LinkedList();
 l1.append(1);
 l1.append(2);
 l1.append(3);
+l1.append(4);
+
+console.log(l1);
+
+l1.prepend(5);
 
 console.log(l1);
